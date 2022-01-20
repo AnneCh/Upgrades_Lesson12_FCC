111279369b12640742441a69fce550d35d7913ae
AnneC <chretien.an@gmail.com>
2022-01-20 12:04:29 +0100

    end of tutorial

--- scripts/deploy_and_upgrade.py
+++ scripts/deploy_and_upgrade.py
@@ -25,9 +25,7 @@ def main():
     print(f"Deploying to {network.show_active()}")
     box = Box.deploy({"from": account}, publish_source=True)
     # Optional, deploy the ProxyAdmin and use that as the admin contract
-    proxy_admin = ProxyAdmin.deploy(
-        {"from": account},
-    )
+    proxy_admin = ProxyAdmin.deploy({"from": account}, publish_source=True)
 
     # initializer = box.store, 1
     box_encoded_initializer_function = encode_function_data()
@@ -71,3 +69,67 @@ def main():
 
 
 # Now it's time for testing!!
+# worked well on rinkeby, contracts were deployed :
+"""
+anne@AnneSager:~/demos/upgrades$ brownie run scripts/deploy_and_upgrade.py --network rinkeby
+Brownie v1.17.2 - Python development framework for Ethereum
+
+UpgradesProject is the active project.
+
+Running 'scripts/deploy_and_upgrade.py::main'...
+Deploying to rinkeby
+Transaction sent: 0x79fb880f2a3ab4c5e3b7d24212b36920369475ac87f142687eee20770acbae11
+  Gas price: 1.000000029 gwei   Gas limit: 113391   Nonce: 136
+  Box.constructor confirmed   Block: 10023591   Gas used: 103083 (90.91%)
+  Box deployed at: 0xc286719ed89641DC0dC2E2E2F89A7807687b216C
+
+Waiting for https://api-rinkeby.etherscan.io/api to process contract...
+Verification submitted successfully. Waiting for result...
+Verification pending...
+Verification complete. Result: Pass - Verified
+Transaction sent: 0x269eaf06f3b4f87ee45a223b9bbefb84834f5d1b6dc6ad0210229b01e388acba
+  Gas price: 1.000000029 gwei   Gas limit: 534673   Nonce: 137
+  ProxyAdmin.constructor confirmed   Block: 10023594   Gas used: 486067 (90.91%)
+  ProxyAdmin deployed at: 0xF7f390e1c67Ef61D8305eC7ea6d3a0A0AA472C35
+
+Waiting for https://api-rinkeby.etherscan.io/api to process contract...
+Verification submitted successfully. Waiting for result...
+Verification pending...
+Verification pending...
+Verification pending...
+Verification complete. Result: Pass - Verified
+Transaction sent: 0xa7676407ebb9b105a4a2a8475063cfe660c585be2624c9214b3d26257f2aab4c
+  Gas price: 1.000000028 gwei   Gas limit: 1000000   Nonce: 138
+  TransparentUpgradeableProxy.constructor confirmed   Block: 10023600   Gas used: 587516 (58.75%)
+  TransparentUpgradeableProxy deployed at: 0x82CfbCCE1616f658e5c90e4a17Ba30D4FbABa801
+
+Verification submitted successfully. Waiting for result...
+Verification complete. Result: Already Verified
+Proxy deployed to 0x82CfbCCE1616f658e5c90e4a17Ba30D4FbABa801, you can now upgrade to V2
+Transaction sent: 0xe7cb765ff4b537d61aaf48015d7dbee0f88fb7346b4e0f3476c547deac91f657
+  Gas price: 1.000000027 gwei   Gas limit: 57466   Nonce: 139
+  Transaction confirmed   Block: 10023606   Gas used: 51909 (90.33%)
+
+1
+Transaction sent: 0x2e156e47fea4226c2a851199c826c40887ab9c6b9306e413cbcf3084f81e07fc
+  Gas price: 1.000000026 gwei   Gas limit: 146012   Nonce: 140
+  BoxV2.constructor confirmed   Block: 10023607   Gas used: 132739 (90.91%)
+  BoxV2 deployed at: 0x26D4CCD87aB6b820Eb5D90e15d572FD6e3D710F7
+
+Waiting for https://api-rinkeby.etherscan.io/api to process contract...
+Verification submitted successfully. Waiting for result...
+Verification pending...
+Verification complete. Result: Already Verified
+Transaction sent: 0x250e66b1604a0cf54030a1aea438a7a7fa85907c0e19df750ebf028ca1e2d029
+  Gas price: 1.000000027 gwei   Gas limit: 42787   Nonce: 141
+  ProxyAdmin.upgrade confirmed   Block: 10023612   Gas used: 38760 (90.59%)
+
+  ProxyAdmin.upgrade confirmed   Block: 10023612   Gas used: 38760 (90.59%)
+
+Proxy has been upgraded to V2!
+Transaction sent: 0x16f0bf17c480dc7b7ce1d317d7ab52bdd5b46a539790b640fabf3259cff09585
+  Gas price: 1.000000025 gwei   Gas limit: 38218   Nonce: 142
+  Transaction confirmed   Block: 10023613   Gas used: 34682 (90.75%)
+
+2
+"""
